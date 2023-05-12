811f7650c97efac22f382f72786300c014bbb408
Fernando <f.nlacerda5@gmail.com>
2023-05-12 13:59:08 -0300

    .

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ using namespace std;
 #include "./hashing/enderecamento-aberto-sondagem-linear.h"
 #include "./testes/funcoes-de-teste.h"
 
+
+void Conjuntos(int, int, string, int);
+void ArquivoInteiro(int, int);
+
+
+
 int main()
 {
   float alfa = 2;
@@ -35,7 +41,7 @@ int main()
   HashSize = 1001;
   SetAmount = 100;
 
-  int Caso = 1;
+  int Caso = 2;
 
   switch (Caso)
   {
@@ -67,6 +73,23 @@ int main()
     break;
   
   case 2:
+
+    Resultados.open("Saida-explicito-estatico-total.csv");
+    Resultados << "\"Conjunto\",\"Alfa\",\"Acesso-Medio\"" << endl;
+    Resultados.close();
+
+    Resultados.open("Saida-explicito-dinamico-total.csv");
+    Resultados << "\"Conjunto\",\"Alfa\",\"Acesso-Medio\"" << endl;
+    Resultados.close();
+
+    Resultados.open("Saida-Duplo-hashing-total.csv");
+    Resultados << "\"Conjunto\",\"Alfa\",\"Acesso-Medio\"" << endl;
+    Resultados.close();
+
+    Resultados.open("Saida-Sondagem-linear-total.csv");
+    Resultados << "\"Conjunto\",\"Alfa\",\"Acesso-Medio\"" << endl;
+    Resultados.close();
+
     for (int j = 1; j < 50; j++)
     {
       ArquivoInteiro(alfa*j, SetAmount);
@@ -92,22 +115,22 @@ void Conjuntos(int Alfa, int SetNumber, string TestFileName, int HashSize){
   cout << "Media de acessos dos metodos para o conjunto " << SetNumber + 1 << endl;
 
   cout << "Encadeamento explicito estatico: " << setw(7) << vetor[0] << endl;
-  Resultados.open("Saida-explicito-estatico.txt", ios_base::app);
+  Resultados.open("Saida-explicito-estatico.csv", ios_base::app);
   Resultados << SetNumber << "," << Alfa << "," << vetor[0] << endl;
   Resultados.close();
 
   cout << "Encadeamento explicito dinamico: " << setw(7) << vetor[1] << endl;
-  Resultados.open("Saida-explicito-dinamico.txt", ios_base::app);
+  Resultados.open("Saida-explicito-dinamico.csv", ios_base::app);
   Resultados << SetNumber << "," << Alfa << "," << vetor[1] << endl;
   Resultados.close();
 
   cout << left << setw(31) << "Duplo hashing" << ": " << right << setw(7) << vetor[2] << endl;
-  Resultados.open("Saida-Duplo-hashing.txt", ios_base::app);
+  Resultados.open("Saida-Duplo-hashing.csv", ios_base::app);
   Resultados << SetNumber << "," << Alfa << "," << vetor[2] << endl;
   Resultados.close();
 
   cout << left << setw(31) << "Sondagem linear" << ": " << right << setw(7) << vetor[3] << endl;
-  Resultados.open("Saida-Sondagem-linear.txt", ios_base::app);
+  Resultados.open("Saida-Sondagem-linear.csv", ios_base::app);
   Resultados << SetNumber << "," << Alfa << "," << vetor[3] << endl;
   Resultados.close();
 
@@ -116,12 +139,32 @@ void Conjuntos(int Alfa, int SetNumber, string TestFileName, int HashSize){
 
 void ArquivoInteiro(int Alfa, int SetAmount){
   float *vetor;
+  ofstream Resultados;
+
+
+
   vetor = MediaDeAcessosArquivo(Alfa, SetAmount);
   cout << fixed << setprecision(2);
   cout << "=-=-=-=-=-=-=-=alfa-" << Alfa << "=-=-=-=-=-=-=-=" << endl;
   cout << "=-=-=-Media de acessos dos metodos=-=-=-" << endl;
+
   cout << "Encadeamento explicito estatico: " << setw(7) << vetor[0] << endl;
+  Resultados.open("Saida-explicito-estatico-total.csv", ios_base::app);
+  Resultados << Alfa << "," << vetor[0] << endl;
+  Resultados.close();
+
   cout << "Encadeamento explicito dinamico: " << setw(7) << vetor[1] << endl;
+  Resultados.open("Saida-explicito-dinamico-total.csv", ios_base::app);
+  Resultados << Alfa << "," << vetor[1] << endl;
+  Resultados.close();
+
   cout << left << setw(31) << "Duplo hashing" << ": " << right << setw(7) << vetor[2] << endl;
+  Resultados.open("Saida-Duplo-hashing-total.csv", ios_base::app);
+  Resultados << Alfa << "," << vetor[2] << endl;
+  Resultados.close();
+
   cout << left << setw(31) << "Sondagem linear" << ": " << right << setw(7) << vetor[3] << endl;
+  Resultados.open("Saida-Sondagem-linear-total.csv", ios_base::app);
+  Resultados << Alfa << "," << vetor[3] << endl;
+  Resultados.close();
 }
